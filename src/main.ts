416c892fa6ae731a7e5378aa92738010b74602ce
Jannatin Naim <jannatinnaim@pondit.family>
2025-09-10 14:53:27 +0600

    cleanup messages

--- src/main.ts
+++ src/main.ts
@@ -14,33 +14,24 @@ async function main() {
 
     await discord.login(process.env.DISCORD_ACCESS_SECRET!);
 
-    const ecoflow = new RestClient({
-        accessKey: process.env.ECOFLOW_ACCESS_KEY!,
-        secretKey: process.env.ECOFLOW_ACCESS_SECRET!,
-        host: "https://api-e.ecoflow.com",
-    });
+    async function schedule() {
+        const ecoflow = new RestClient({
+            accessKey: process.env.ECOFLOW_ACCESS_KEY!,
+            secretKey: process.env.ECOFLOW_ACCESS_SECRET!,
+            host: "https://api-e.ecoflow.com",
+        });
 
-    // await ecoflow.getMqttCredentials();
-
-    // return;
-
-    setInterval(async () => {
         const delta2Properties = await ecoflow.getDevicePropertiesPlain(
             process.env.ECOFLOW_DELTA_2_SN! as `R331${string}`
         );
 
         const inputWatts = delta2Properties.data?.["pd.wattsInSum"];
         const outputWatts = delta2Properties.data?.["pd.wattsOutSum"];
-        // const isGridOnline = delta2Properties.data?.["bms_emsStatus.chgState"];
+
         const isCharging = inputWatts > 200;
         const batteryLevel = delta2Properties.data?.["bms_bmsStatus.soc"];
-        console.log({
-            inputWatts,
-            outputWatts,
-            // isGridOnline,
-            isCharging,
-            batteryLevel,
-        });
+
+        console.log({ inputWatts, outputWatts, isCharging, batteryLevel });
 
         await discord.user?.fetch();
         await discord.user?.setPresence({});
@@ -51,22 +42,15 @@ async function main() {
                 activities: [{ type: ActivityType.Watching, name: `the battery at ${batteryLevel}%` }],
             });
         } else {
-            // if (isGridOnline) {
-            if (true) {
-                await discord.user?.setPresence({
-                    status: PresenceUpdateStatus.DoNotDisturb,
-                    activities: [{ type: ActivityType.Listening, name: `to crickets at ${batteryLevel}%` }],
-                });
-            } else {
-                // await discord.user?.setPresence({
-                //     status: PresenceUpdateStatus.Idle,
-                //     activities: [
-                //         { type: ActivityType.Competing, name: `with Nigerians for lowest voltage electricity` },
-                //     ],
-                // });
-            }
+            await discord.user?.setPresence({
+                status: PresenceUpdateStatus.DoNotDisturb,
+                activities: [{ type: ActivityType.Listening, name: `to crickets at ${batteryLevel}%` }],
+            });
         }
-    }, 5000);
+    }
+
+    await schedule();
+    setInterval(schedule, 5000);
 }
 
 config();
